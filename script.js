c9230d97aa706d1b2bde1f898fcc9fcd54ac28ff
Adina Pollack <adinap16@gmail.com>
2016-08-05 13:38:03 -0400

    responsive af

--- script.js
+++ script.js
@@ -21,10 +21,60 @@ $(document).ready(function(){
 
   $("#tacoImage").hide();
   $("#gigImage").hide();
-  
+
   $(".needtacos").on("mouseover", showTacoPhoto);
   $(".needtacos").on("mouseout", hideTacoPhoto);
 
   $(".gigMappr").on("mouseover", showGigPhoto);
   $(".gigMappr").on("mouseout", hideGigPhoto);
+
+
+
+
+
+  $( ".cross" ).hide();
+  $(".hamburger").hide();
+
+  $(window).resize(function(){
+    if ($(window).width() < 600){
+      $(".hamburger").show();
+      $( ".nav" ).hide();
+
+
+      $(".hamburger").off("click");
+      $( ".hamburger" ).click(function() {
+        $( ".nav" ).slideToggle( "fast", function() {
+          $( ".hamburger" ).hide();
+          $( ".cross" ).show();
+          $("#divider").hide();
+          $(".hero").css("padding-top", "480px")
+        });
+      });
+
+      $(".cross").off("click");
+      $( ".cross" ).click(function() {
+        $( ".nav" ).slideToggle( "fast", function() {
+          $( ".cross" ).hide();
+          $( ".hamburger" ).show();
+          $("#divider").show();
+          $(".hero").css("padding-top", "250px");
+        });
+      });
+
+
+      $("li").on("click", function(){
+        $( ".nav" ).hide();
+        $( ".cross" ).hide();
+        $( ".hamburger" ).show();
+        $("#divider").show();
+        $("section").css("padding-top", "150px");
+        $(".hero").css("padding-top", "200px");
+      });
+    } else{
+      $( ".cross" ).hide();
+      $(".hamburger").hide();
+      $(".nav").show();
+    }
+  })
+
 });
