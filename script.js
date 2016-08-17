8cc37ab5c535506815a955053e521b181e4392d6
Adina Pollack <adinap16@gmail.com>
2016-08-17 15:21:05 -0400

    new images and redo projects section

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 $(document).ready(function(){
-  function showTacoPhoto(){
-    $("#tacoImage").show();
-    $("#tacoTitle").hide();
-  }
-
-  function hideTacoPhoto(){
-    $("#tacoImage").hide();
-    $("#tacoTitle").show();
-  }
-
-  function showGigPhoto(){
-    $("#gigImage").show();
-    $("#gigTitle").hide();
-  }
-
-  function hideGigPhoto(){
-    $("#gigImage").hide();
-    $("#gigTitle").show();
-  }
-
-  $("#tacoImage").hide();
-  $("#gigImage").hide();
-
-  $(".needtacos").on("mouseover", showTacoPhoto);
-  $(".needtacos").on("mouseout", hideTacoPhoto);
-
-  $(".gigMappr").on("mouseover", showGigPhoto);
-  $(".gigMappr").on("mouseout", hideGigPhoto);
+  // function showTacoPhoto(){
+  //   $("#tacoImage").show();
+  //   $("#tacoTitle").hide();
+  // }
+  //
+  // function hideTacoPhoto(){
+  //   $("#tacoImage").hide();
+  //   $("#tacoTitle").show();
+  // }
+  //
+  // function showGigPhoto(){
+  //   $("#gigImage").show();
+  //   $("#gigTitle").hide();
+  // }
+  //
+  // function hideGigPhoto(){
+  //   $("#gigImage").hide();
+  //   $("#gigTitle").show();
+  // }
+  //
+  // $("#tacoImage").hide();
+  // $("#gigImage").hide();
+  //
+  // $(".needtacos").on("mouseover", showTacoPhoto);
+  // $(".needtacos").on("mouseout", hideTacoPhoto);
+  //
+  // $(".gigMappr").on("mouseover", showGigPhoto);
+  // $(".gigMappr").on("mouseout", hideGigPhoto);
 
   $( ".cross" ).hide();
   $(".hamburger").hide();
